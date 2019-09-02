d7504baac199da85ff0e8e4e2bdfbca292273d1f
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-09-02 16:30:48 -0400

    2.10

--- Week 2/ViewController.swift
+++ Week 2/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
     
     //code below first executes when the app's view first loads
     override func viewDidLoad() {
@@ -20,20 +21,36 @@ class ViewController: UIViewController {
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
         
-        let message1 = "You are awesome!"
-        let message2 = "You are great!"
-        let message3 = "You are amazing!"
-        
-        if messageLabel.text == message1{
-            messageLabel.text = message2
-        }
-        else if messageLabel.text == message2{
-            messageLabel.text = message3
+//        let message1 = "You are awesome!"
+//        let message2 = "You are great!"
+//        let message3 = "You are amazing!"
+//
+//        if messageLabel.text == message1{
+//            messageLabel.text = message2
+//        }
+//        else if messageLabel.text == message2{
+//            messageLabel.text = message3
+//        }
+//        else{
+//            messageLabel.text = message1
+//        }
+        let messages = ["You are awesome!",
+                        "You are great!",
+                        "You are fantastic!",
+                        "When the genius bar needs help, they call you",
+                        "You brighten my day!",
+                        "You da bomb!",
+                        "Hey, fabulous!",
+                        "You are tremendous!",
+                        "You got the skills of Jony Ive!",
+                        "I can't wait to download your app!"]
+        messageLabel.text=messages[index]
+        if index == messages.count-1{
+            index = 0
         }
         else{
-            messageLabel.text = message1
+        index+=1
         }
-        
     }
 
 }
