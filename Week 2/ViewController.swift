009d89227871521048367b0f080b55bb191a7569
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-09-08 13:41:41 -0400

    2.14

--- Week 2/ViewController.swift
+++ Week 2/ViewController.swift
@@ -49,7 +49,9 @@ class ViewController: UIViewController {
         
         //messageLabel.text = messages.randomElement()!
         
-        var newIndex = -1
+        //var newIndex = -1
+        var newIndex: Int
+        
         repeat{
             newIndex = Int.random(in: 0..<messages.count)
         }while index==newIndex
