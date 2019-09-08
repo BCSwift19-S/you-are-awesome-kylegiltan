4701e09e43405f28b403c020941ceb02dacb7fd3
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-09-08 12:30:16 -0400

    2.12

--- Week 2/ViewController.swift
+++ Week 2/ViewController.swift
@@ -46,13 +46,16 @@ class ViewController: UIViewController {
                         "You are tremendous!",
                         "You got the skills of Jony Ive!",
                         "I can't wait to download your app!"]
-        messageLabel.text=messages[index]
-        if index == messages.count-1{
-            index = 0
-        }
-        else{
-        index+=1
-        }
+        
+        messageLabel.text = messages.randomElement()!
+        
+//        messageLabel.text=messages[index]
+//        if index == messages.count-1{
+//            index = 0
+//        }
+//        else{
+//        index+=1
+//        }
     }
 
 }
